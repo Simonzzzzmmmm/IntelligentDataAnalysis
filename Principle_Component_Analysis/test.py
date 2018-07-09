d24aff438feaa8406fa3751540ddd541358fd1d0
Simonzzzzmmmm <11510695@mail.sustc.edu.cn>
2018-07-09 23:05:52 +0800

    add clustering for two dimension data, little bug for the initial central points

--- Principle_Component_Analysis/test.py
+++ Principle_Component_Analysis/test.py
@@ -2,9 +2,11 @@ from sklearn.datasets import load_boston
 import numpy as np
 import scipy.linalg
 from sklearn import preprocessing
+from numpy.linalg import cholesky
 from sklearn.preprocessing import Imputer
 import matplotlib.pyplot as plt
 import pandas as pd
+import random
 def normalization(xis):
     for i in range(xis.shape[1]):
         # print(xis[:,i])
@@ -17,7 +19,7 @@ def normalization(xis):
             xis[:,i] = (xis[:,i]-mean)/np.abs(std_variance)
     return xis
 def get_C(xis):
-    return np.dot(np.transpose(xis),xis)/len(xis)
+    return (np.dot(np.transpose(xis),xis))/len(xis)
     # return np.cov(np.array(xis,dtype=float))
 def get_eigen(covariance):
     return np.linalg.eig(covariance)
@@ -51,7 +53,6 @@ def PCA(eigenvalue,xis):
     return new_axis
 def data_pruning_for_school_explorer():
     data_set = pd.read_csv("2016 School Explorer.csv")
-    titles = data_set.columns
     data_set.drop(data_set.columns[0:6],axis = 1,inplace = True)
     data_set.drop("Address (Full)",axis = 1,inplace = True)
     data_set.drop("City",axis = 1,inplace = True)
@@ -67,49 +68,121 @@ def data_pruning_for_school_explorer():
     data_set.drop("Percent of Students Chronically Absent",axis = 1,inplace = True)
     data_set.drop("Rigorous Instruction Rating",axis = 1,inplace = True)
     data_set.drop("Grade Low",axis = 1,inplace = True)
+    data_set.drop("Grade High",axis = 1,inplace=True)
     data_set = list(data_set.values)
     for i in range(len(data_set)):
-        if (type(data_set[i][4])=="float"):
-            #数据集的索引4需要转化为float
-            data_set[i][4] = float(data_set[i][4])
-        else:
-            data_set[i][4] = 0.0
+        # if (isinstance(data_set[i][4], (int, float))):
+        #     if (data_set[i][4]==data_set[i][4]):
+        #         #数据集的索引4需要转化为float
+        #         data_set[i][4] = float(data_set[i][4])
+        #     else:
+        #         data_set[i][4] = 1.0
+        # else:
+        #     data_set[i][4] = 1.0
         #数据集的索引6需要去掉美元符号和逗号并且转化为float
-        if_nan = (data_set[i][6]==data_set[i][6])
+        if_nan = (data_set[i][5]==data_set[i][5])
         if (if_nan==False):
-            data_set[i][6] = 0.0
+            data_set[i][5] = 0.0
         else:
-            data_set[i][6]= data_set[i][6].replace("$","")
-            data_set[i][6]= float(data_set[i][6].replace(",",""))
+            data_set[i][5]= data_set[i][5].replace("$","")
+            data_set[i][5]= float(data_set[i][5].replace(",",""))
         #from column 7 to column 18
-        for j in range(7,19):
+        for j in range(6,18):
             #数据集的索引7-18需要去掉百分符号并且转化为float
             if_nan1 = (data_set[i][j]==data_set[i][j])
             if(if_nan1==False):
                 data_set[i][j] = 0.0
             else:
                 data_set[i][j]=float(data_set[i][j].replace("%",""))/100 
-    
     data_set = np.array(data_set)
     data_set = normalization(data_set)
     data_set = mean_preprocessing(data_set)
     return data_set
+def k_means_clustering(k,data_set):
+    k_points = [[]for i in range(k)]
+    #randomly choose k initial center points of clusters, tricky: let data_set be the central points
+    def init(k):
+        # initial_center = np.zeros((k,len(data_set[0])))
+        initial_center = []
+        for i in range(k):
+            random_index = random.randrange(1,len(data_set))
+            initial_center.append(data_set[random_index])
+        return initial_center
+    def Elucidean_distance(X1,X2):
+        dist = np.linalg.norm(X1-X2)
+        return dist
+    # def cal_cost(initial_center,data_set):
+    #     sum = 0 
+    #     for i in len(data_set):
+    #         temp = data_set[i]-initial_center[i]
+    #         sum += float(np.dot(np.transpose(temp),(temp)))
+    #     return sum
+    # 计算平均数
+    def averagenum(num,j):
+        nsum = 0
+        for i in range(len(num)):
+            nsum += num[i][j]
+        return nsum / len(num)
+    def find_clusters(point,initial_center):
+        min_index = 9999
+        min_distance = 9999
+        for i in range(len(initial_center)):
+            distance = Elucidean_distance(point,initial_center[i])
+            if (distance<min_distance):
+                min_distance = distance
+                min_index = i
+        return [min_index,min_distance]
+    clusters = [[] for i in range(k)]
+    initial_center = init(k)
+    step = 0
+    while True: 
+        for i in range(len(data_set)):
+            #find the cluster which is nearest to the point
+            min_index,min_distance = find_clusters(data_set[i],initial_center)
+            #add the points to the corresponding cluster
+            clusters[min_index].append(data_set[i])
+        for i in range(len(initial_center)):
+            #for every dimension
+            for j in range(len(initial_center[0])):
+                #the ith cluster's central point, change initial central points
+                initial_center[i][j] = averagenum(data1,j)
+        if (step<500):
+            break
+        else:
+            step+=1
+    plt.plot(data1[:, 0], data1[:, 1], '+')
+    for i in range(len(initial_center)):
+        plt.plot(initial_center[i][0],initial_center[i][1],'x')
+    plt.show()
+
 if __name__=="__main__":
     
     data_set = data_pruning_for_school_explorer()
     C = get_C(data_set)
     eigenvalue,eigenvector = get_eigen(C)
+    # eigenvalue = np.array(eigenvalue,dtype=float)
     #do principle component analysis
     new_data_set = PCA(eigenvalue,data_set)
-    print(new_data_set.shape)
-    plt.show()
+    new_dimension_data = get_new_points(data_set,eigenvector)
+    # print (data_set)
+    # print (new_dimension_data)
+    
+    # plt.show()
     # data_set = normalization(data_set)
     # co_variance_matrix = get_C(data_set)
     # eigenvalue,eigenvector = get_eigen(co_variance_matrix)
     # plt.plot(eigenvalue,'.')
     # plt.show()
-    
-    
-    # data_value = normalization(data_value)
-    
+
+    data1 = np.random.randn(500, 2)
+    for i in range(len(data1)):
+        data1[i][0] = data1[i][0]+100
+    data2 = np.random.randn(500, 2)-0
+    data3 = np.random.randn(500, 2)-100
+    data1 = np.append(data1,data2,axis = 0)
+    data1 = np.append(data1,data3,axis = 0)
+    k_means_clustering(4,data1)
+    # plt.plot(data1[:,0],data1[:,1],'+')
+    # plt.show()
+
 
